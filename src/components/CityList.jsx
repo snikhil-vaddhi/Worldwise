ae1d3ac747656b60d8817b4e8c62e25bffb217ba
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-05-22 22:46:22 -0500

    resolved an error

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -9,7 +9,7 @@ function CityList() {
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
-      <Message message="{ Add your first city by clicking on a city on the map }" />
+      <Message message=" Add your first city by clicking on a city on the map" />
     );
   return (
     <ul className={styles.cityList}>
